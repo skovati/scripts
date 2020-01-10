427a3a4a2053fde9a5ff35f331bd880980d3bbb3
Luke Jurgella <lukejurgella@gmail.com>
2020-01-10 13:12:57 -0600

    Add cancel_test.sh option to main disk_testing.sh script

--- disk_testing.sh
+++ disk_testing.sh
@@ -4,11 +4,12 @@ ans=1
 
 while [ $ans -ne 0 ]; do
 
-echo -n "What would you like to do? (0 to exit):
+echo "What would you like to do? (0 to exit):
 1: Initialize disks in raid0
 2: Run a health check
 3: Run short tests
-4: Run extended test "
+4: Run extended test 
+5: Cancel current running tests"
 read ans
 
 case $ans in
@@ -27,6 +28,11 @@ echo "You have chosen to run short tests"
 4)
 echo "You have chosen to run extended tests"
 ./extended_test.sh
+;;
+5)
+echo "You have chosen to cancel all running tests"
+./cancel_test.sh
+;;
 esac
 done
 
